ed6341cdb5cc39b4787f9c165299575df47c7f75
Jalynn Chang <89932840+jmchang35@users.noreply.github.com>
2022-04-25 18:40:53 -0400

    Update late.js

--- late.js
+++ late.js
@@ -1,1 +1,10 @@
-// start coding your function here!
+function running_late(date) {
+    const date = new Date(date);
+
+    if (date.getHours() >= 22) {
+        return "It is late!"
+    }
+    return "It is still early!"
+}
+
+exports.running_late = running_late 
